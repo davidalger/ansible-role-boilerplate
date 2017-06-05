bed8623f4fd4be7f2bfe2b6ffa5450fb3f647303
David Alger <davidmalger@gmail.com>
2017-06-05 15:51:53 -0500

    Updated boilerplate, sshd-lockdown and redis roles to properly deploy templates with the ansible_managed headers

--- templates/profile.d/classyllama.sh
+++ templates/profile.d/classyllama.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
-##
- # This file was deployed via Classy Llama managed services. Please DO NOT MODIFY this file.
- ##
+# {{ ansible_managed_header }}
+# {{ ansible_managed }}
 
-# Fancy PS1 with flashing username / red path for root login
+# Configure PS1 with flashing username and red path for root login
 if [[ $EUID -ne 0 ]]; then
     export PS1='\[\033[0;36m\]\u@\h\[\033[0m\]:\@:\[\033[0;37m\]\w\[\033[0m\]$ '
 else
     export PS1='\[\033[0;5m\]\u@\h\[\033[0m\]:\@:\[\033[0;31m\]\w\[\033[0m\]# '
 fi
 
-# Color output
+# Enable full-color shell output
 export CLICOLOR=1
 export LSCOLORS=ExFxCxDxBxegedabagacad
 
-# Allow for easy searching of history (but only bind when we have a tty)
+# Setup history search ability (only bind when a tty is present)
 if [ -t 1 ]; then
     bind '"\e[A":history-search-backward'
     bind '"\e[B":history-search-forward'
